6c9bfa58dafb12c16de805b337ffe693d6429fb8
PhungMHieu <kiritosad41@gmail.com>
2025-08-25 20:12:55 +0700

    Initial Commit

---
+++ FinalProjectSwiftUI/FinalProjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectSwiftUIApp.swift
+//  FinalProjectSwiftUI
+//
+//  Created by Admin on 25/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
